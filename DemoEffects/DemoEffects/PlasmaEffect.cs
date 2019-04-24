ff38de99b9298bc3544cfce02cd7daccccb4103a
xSillusx <tkattanek00@gmail.com>
2019-04-24 20:38:21 +0200

    sfml init

---
+++ DemoEffects/DemoEffects/PlasmaEffect.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEffects
+{
+    class PlasmaEffect
+    {
+    }
+}
---
+++ DemoEffects/DemoEffects/Program.cs
@@ -0,0 +1,46 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEffects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Init();
+
+        }
+
+        public static void Init()
+        {
+            RenderWindow app = new RenderWindow(new VideoMode(800, 600), "Demo Effects");
+            app.Closed += new EventHandler(OnClose);
+
+            while (app.IsOpen)
+            {
+                app.DispatchEvents();
+                app.Clear(Color.Blue);
+
+
+                // effect code here
+
+
+
+
+                // Update the window
+                app.Display();
+            }
+        }
+
+        private static void OnClose(object sender, EventArgs e)
+        {
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+    }
+}
